809e3003f8fca723a0fba602948fe5278bc7f208
Mateo de Mayo <mateodemayo@gmail.com>
2019-10-25 19:38:28 -0300

    Add svg2png support for a single icon conversion

--- svg2png.sh
+++ svg2png.sh
@@ -3,6 +3,17 @@
 DIR=$(dirname $(realpath $0))
 SVG_PATH="${DIR}/svg-icons"
 PNG_PATH="${DIR}/Matter/icons"
+CONVERT="convert \
+    -trim \
+    -scale 36x36 \
+    -extent 72x72 \
+    -gravity center \
+    -define png:color-type=6 \
+    -background none \
+    -colorspace sRGB \
+    -channel RGB \
+    -threshold -1 \
+    -density 300"
 
 while [ "$1" != "" ]; do
     case $1 in
@@ -10,8 +21,14 @@ while [ "$1" != "" ]; do
         echo "Usage: $0"
         echo
         echo "Options:"
-        echo "  -h --help         Display this help and exit"
+        echo -e "  -h, --help\tDisplay this help and exit"
+        echo -e "  -i, --icon\tSelect a specific icon name to convert"
         exit 0
+        ;;
+    -i | --install)
+        ICON="$2"
+        shift # past argument
+        shift # past value
         ;;
     esac
     shift
@@ -26,19 +43,15 @@ fi
 echo "Starting conversion ..."
 
 cd ${SVG_PATH}
-for file in *.svg; do
-    echo "Processing ${file}"
-    convert -trim \
-        -scale 36x36 \
-        -extent 72x72 \
-        -gravity center \
-        -define png:color-type=6 \
-        -background none \
-        -colorspace sRGB \
-        -channel RGB \
-        -threshold -1 \
-        -density 300 \
-        ${file} "${PNG_PATH}/${file%%.*}.png"
-done
+
+if [[ -z "$ICON" ]]; then
+    for file in *.svg; do
+        echo "Processing ${file}"
+        $CONVERT ${file} "${PNG_PATH}/${file%%.*}.png"
+    done
+else
+    echo "Processing ${ICON}"
+    $CONVERT ${ICON}.svg "${PNG_PATH}/${ICON}.png"
+fi
 
 echo "Finished!"
