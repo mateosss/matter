dd14f8e5af30595eb24ef9e9f65bc066da293c58
Mateo de Mayo <mateodemayo@gmail.com>
2020-06-26 21:37:17 -0300

    Add uninstall option, argparse and more info prints

--- matter.py
+++ matter.py
@@ -3,6 +3,7 @@
 import sys
 import os
 import re
+from argparse import ArgumentParser
 from os.path import dirname, isdir
 from subprocess import run, check_call
 from shutil import which, rmtree, copytree
@@ -12,7 +13,8 @@ from shutil import which, rmtree, copytree
 MIN_PYTHON_VERSION = (3, 6)  # Mainly for f-strings
 
 THEME_NAME = "Matter"
-INSTALLER_DIR = dirname(os.path.realpath(__file__))
+INSTALLER_NAME = __file__
+INSTALLER_DIR = dirname(os.path.realpath(INSTALLER_NAME))
 INSTALLATION_SOURCE_DIR = f"{INSTALLER_DIR}/{THEME_NAME}"
 INSTALLATION_TARGET_DIR = f"/boot/grub/themes/{THEME_NAME}"
 
@@ -20,6 +22,12 @@ GRUB_DEFAULTS_PATH = f"/etc/default/grub"
 
 THEME_TEMPLATE_PATH = f"{INSTALLER_DIR}/theme.txt.template"
 GRUB_DEFAULTS_TEMPLATE_PATH = f"{INSTALLER_DIR}/grub.template"
+
+THEME_OVERRIDES_TITLE = f"{THEME_NAME} Theme Overrides"
+BEGIN_THEME_OVERRIDES = f"### BEGIN {THEME_OVERRIDES_TITLE}"
+END_THEME_OVERRIDES = f"### END {THEME_OVERRIDES_TITLE}"
+
+# Utils
 
 
 def sh(command):
@@ -29,13 +37,6 @@ def sh(command):
 
 def has_command(command):
     return which(command) is not None
-
-
-def update_grub_cfg():
-    COMMAND = "update-grub"
-    print("[Info] Making grub.cfg with update-grub...")
-    assert has_command(COMMAND)
-    sh(COMMAND)
 
 
 def check_python_version():
@@ -49,19 +50,57 @@ def check_python_version():
 
 def check_root_or_prompt():
     if os.geteuid() != 0:
-        print(f"{THEME_NAME} installer needs root access.")
+        print(f"[Info] Request root access.")
         exit_code = sh("sudo -v")
         if exit_code != 0:
             raise Exception(
                 "[Error] Could not verify root access, you could try with sudo."
             )
         # Relaunch the program with sudo
-        child_exit_code = sh(f"sudo {INSTALLER_DIR}/matter.py")
+        args = " ".join(sys.argv[1:])
+        child_exit_code = sh(f"sudo {INSTALLER_DIR}/{INSTALLER_NAME} {args}")
         exit(child_exit_code)  # Propagate exit code
 
 
-def prepare_source_folder():
-    "Applies any transformation related to the source folder to copy from"
+def delete_dir(directory):
+    if isdir(directory):
+        rmtree(directory)
+
+
+def read_cleaned_grub_defaults():
+    # Read previous defaults
+    with open(GRUB_DEFAULTS_PATH, "r") as f:
+        grub_defaults = f.read()
+
+    # Remove previous theme defaults
+    cleaned_grub_defaults = re.sub(
+        f"\n*{BEGIN_THEME_OVERRIDES}.*{END_THEME_OVERRIDES}\n*",
+        "",
+        grub_defaults,
+        flags=re.DOTALL,
+    )
+    return cleaned_grub_defaults
+
+
+def parse_args():
+    parser = ArgumentParser()
+    parser.add_argument(
+        "--uninstall", "-u", action="store_true", help=f"uninstall {THEME_NAME}",
+    )
+    return parser.parse_args()
+
+
+# Main procedures
+
+
+def clean_install_dir():
+    print("[Info] Clean install directory.")
+    if isdir(INSTALLATION_TARGET_DIR):
+        rmtree(INSTALLATION_TARGET_DIR)
+
+
+def prepare_source_dir():
+    print("[Info] Build theme from user preferences.")
 
     # Temporary settings until user args are implemented
     highlight = "#E91E63"  # pink
@@ -85,27 +124,26 @@ def prepare_source_folder():
         f.write(parsed_theme)
 
 
-def prepare_target_folder():
-    "Applies any transformation related to the target installation folder"
-    # Clean installation dir
-    if isdir(INSTALLATION_TARGET_DIR):
-        rmtree(INSTALLATION_TARGET_DIR)
+def prepare_target_dir():
+    print("[Info] Prepare installation directory.")
+    clean_install_dir()
 
 
 def copy_source_to_target():
+    print("[Info] Copy built theme to installation directory.")
     copytree(INSTALLATION_SOURCE_DIR, INSTALLATION_TARGET_DIR)
 
 
-def update_grub_defaults():
-    # Read previous defaults
-    with open(GRUB_DEFAULTS_PATH, "r") as f:
-        grub_configs = f.read()
+def update_grub_cfg():
+    COMMAND = "update-grub"
+    print(f"[Info] Remake grub.cfg with {COMMAND}.")
+    assert has_command(COMMAND)
+    sh(COMMAND)
 
-    # Remove previous theme defaults
-    tag_title = f"{THEME_NAME} Theme Overrides"
-    begin_tag = f"### BEGIN {tag_title}"
-    end_tag = f"### END {tag_title}"
-    grub_configs = re.sub(f"\n*{begin_tag}.*{end_tag}\n*", "", grub_configs, flags=re.DOTALL)
+
+def update_grub_defaults():
+    print(f"[Info] Patch {GRUB_DEFAULTS_PATH} with {THEME_OVERRIDES_TITLE}.")
+    grub_configs = read_cleaned_grub_defaults()
 
     # Parse grub defaults template, append parsed contents, and write back
 
@@ -114,20 +152,45 @@ def update_grub_defaults():
 
     context = {"installation_dir": INSTALLATION_TARGET_DIR}
     parsed_extra_grub = template.format(**context)
-    grub_configs += f"\n\n{begin_tag}\n{parsed_extra_grub}\n{end_tag}\n\n"
+    grub_configs += (
+        f"\n\n{BEGIN_THEME_OVERRIDES}\n{parsed_extra_grub}\n{END_THEME_OVERRIDES}\n\n"
+    )
 
     with open(GRUB_DEFAULTS_PATH, "w") as f:
         f.write(grub_configs)
 
 
-if __name__ == "__main__":
-    check_python_version()
+def clean_grub_defaults():
+    print(f"[Info] Clean {THEME_OVERRIDES_TITLE}.")
+    cleaned_grub_defaults = read_cleaned_grub_defaults()
+    with open(GRUB_DEFAULTS_PATH, "w") as f:
+        f.write(cleaned_grub_defaults)
+
+
+def do_install():
+    print(f"[Info] Begin {THEME_NAME} install.")
     check_root_or_prompt()
-
-    prepare_source_folder()
-    prepare_target_folder()
-
+    prepare_source_dir()
+    prepare_target_dir()
     copy_source_to_target()
-
     update_grub_defaults()
     update_grub_cfg()
+    print(f"{THEME_NAME} succesfully installed.")
+
+
+def do_uninstall():
+    print(f"[Info] Begin {THEME_NAME} uninstall.")
+    check_root_or_prompt()
+    clean_grub_defaults()
+    clean_install_dir()
+    update_grub_cfg()
+    print(f"{THEME_NAME} succesfully uninstalled.")
+
+
+if __name__ == "__main__":
+    check_python_version()
+    args = parse_args()
+    if args.uninstall:
+        do_uninstall()
+    else:
+        do_install()
